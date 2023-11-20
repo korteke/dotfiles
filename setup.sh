1eb7f2a564267d77297e32922f7862f05a43aedb
Keijo Korte <git@kvak.net>
2023-11-20 16:55:47 +0200

    Spaceship

--- setup.sh
+++ setup.sh
@@ -47,10 +47,12 @@ if [[ "$OSTYPE" =~ ^linux ]]; then
     sudo apt install fzf ripgrep
 fi
 
-if [ ! -d $ZSH_CUSTOM ]; then
-  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-fi
+echo "${ZSH_CUSTOM}/themes/spaceship-prompt"
+
+#if [ ! -d "${ZSH_CUSTOM}/themes/spaceship-prompt" ]; then
+#  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+#  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+#fi
 
 # tmux plugin manager
 TPMDIR="${HOME}/.tmux/plugins/tpm"
